af8518d25113298e7985a4c1fa694410f098b48b
Tomás Salina <salinatomass53@gmail.com>
2022-09-27 15:08:18 -0300

    Validations with Yup

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,4 +1,5 @@
 import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 import './TaskForm.styles.css'
 
@@ -10,12 +11,20 @@ export const TaskForm = () => {
     descrption: '',
   }
 
+  const required = '* Campo obligatorio'
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().min(6, 'La cantidad minima es 6').required(required),
+    status: Yup.string().required(required),
+    priority: Yup.string().required(required),
+  })
+
   const onSubmit = () => {
     alert('Good')
   }
 
-  const formik = useFormik({ initialValues, onSubmit })
-  const { handleChange, handleSubmit } = formik
+  const formik = useFormik({ initialValues, validationSchema, onSubmit })
+  const { handleChange, handleSubmit, errors } = formik
 
   return (
     <div className="task-form">
@@ -24,23 +33,31 @@ export const TaskForm = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <div>
-            <input type="text" name="title" placeholder="Titulo" />
+            <input
+              type="text"
+              name="title"
+              placeholder="Titulo"
+              onChange={handleChange}
+            />
+            {errors.title && <span>{errors.title}</span>}
           </div>
           <div>
-            <select name="status">
+            <select name="status" onChange={handleChange}>
               <option value="">Seleccionar un estado</option>
               <option value="new">Nueva</option>
               <option value="inProcess">En proceso</option>
               <option value="finished">Terminada</option>
             </select>
+            {errors.status && <span>{errors.status}</span>}
           </div>
           <div>
-            <select name="priority">
+            <select name="priority" onChange={handleChange}>
               <option value="">Seleccionar una prioridad</option>
               <option value="low">Baja</option>
               <option value="medium">Media</option>
               <option value="high">Alta</option>
             </select>
+            {errors.priority && <span>{errors.priority}</span>}
           </div>
         </div>
         <div>
