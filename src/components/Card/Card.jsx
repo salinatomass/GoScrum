b324416d508baa22cde5f0c3b9d614f6dff4f0ce
Tomás Salina <salinatomass53@gmail.com>
2022-10-01 22:51:13 -0300

    Fetching task and Skeleton

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,28 @@
 const Card = ({ data }) => {
-  const { title, description, datetime, creator, type, priority } = data
+  const {
+    title,
+    description,
+    createdAt,
+    user: { userName },
+    status,
+    importance,
+  } = data
+
+  const limitString = str => {
+    if (str.length > 170)
+      return { string: str.slice(0, 167).concat('...'), addButton: true }
+    return { string: str, addButton: false }
+  }
 
   return (
     <article className="card">
       <div className="close">x</div>
       <h3>{title}</h3>
-      <h6>{datetime}</h6>
-      <h5>{creator}</h5>
-      <button type="button">{type}</button>
-      <button type="button">{priority}</button>
-      <p>{description}</p>
+      <h6>{createdAt}</h6>
+      <h5>{userName}</h5>
+      <button type="button">{status.toLowerCase()}</button>
+      <button type="button">{importance.toLowerCase()}</button>
+      <p>{limitString(description).string}</p>
     </article>
   )
 }
--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,22 +1,59 @@
+import { useState, useEffect } from 'react'
 import './Tasks.styles.css'
 import { useResize } from '../../../hooks/useResize'
 import Header from '../../Header/Header'
 import { TaskForm } from '../../TaskForm/TaskForm'
 import Card from '../../Card/Card'
-import { tasksData } from './tasksData'
+
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
 
 export const Tasks = () => {
+  const [list, setList] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
   const { isPhone } = useResize()
 
-  const limitString = str => {
-    if (str.length > 170)
-      return { string: str.slice(0, 167).concat('...'), addButton: true }
-    return { string: str, addButton: false }
+  const renderAllTasks = () =>
+    list.map(task => <Card key={task._id} data={task} />)
+
+  const renderNewTasks = () =>
+    list
+      .filter(task => task.status === 'NEW')
+      .map(task => <Card key={task._id} data={task} />)
+
+  const renderInProgressTasks = () =>
+    list
+      .filter(task => task.status === 'IN PROGRESS')
+      .map(task => <Card key={task._id} data={task} />)
+
+  const renderFinishedTasks = () =>
+    list
+      .filter(task => task.status === 'FINISHED')
+      .map(task => <Card key={task._id} data={task} />)
+
+  const fetchTasks = async () => {
+    setIsLoading(true)
+
+    try {
+      const res = await fetch(`${API_ENDPOINT}/task`, {
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      })
+      const data = await res.json()
+      setList(data.result)
+      setIsLoading(false)
+    } catch (err) {
+      console.log(err)
+      setIsLoading(false)
+    }
   }
 
-  const renderAllTasks = () => {
-    return tasksData.map(task => <Card key={task.id} data={task} />)
-  }
+  useEffect(() => {
+    fetchTasks()
+  }, [])
 
   return (
     <>
@@ -27,22 +64,32 @@ export const Tasks = () => {
           <div className="list_header">
             <h2>Mis tareas</h2>
           </div>
-          {isPhone ? (
-            <div className="list phone">{renderAllTasks()}</div>
+          {isLoading ? (
+            <Skeleton count={10} />
+          ) : isPhone ? (
+            <div className="list phone">
+              {!list.length ? <p>No hay tareas creadas</p> : renderAllTasks()}
+            </div>
           ) : (
             <div className="list_group">
-              <div className="list">
-                <h4>Nuevas</h4>
-                <Card data={tasksData[0]} />
-              </div>
-              <div className="list">
-                <h4>En proceso</h4>
-                <Card data={tasksData[1]} />
-              </div>
-              <div className="list">
-                <h4>Finalizadas</h4>
-                <Card data={tasksData[2]} />
-              </div>
+              {!list.length ? (
+                <p>No hay tareas creadas</p>
+              ) : (
+                <>
+                  <div className="list">
+                    <h4>Nuevas</h4>
+                    {renderNewTasks()}
+                  </div>
+                  <div className="list">
+                    <h4>En proceso</h4>
+                    {renderInProgressTasks()}
+                  </div>
+                  <div className="list">
+                    <h4>Finalizadas</h4>
+                    {renderFinishedTasks()}
+                  </div>
+                </>
+              )}
             </div>
           )}
         </section>
