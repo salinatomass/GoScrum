b7745e7f3fafe705374f448db2cf3c37206a83a5
Tomás Salina <salinatomass53@gmail.com>
2022-10-01 23:30:22 -0300

    list filter by importance and autor

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -5,33 +5,17 @@ import Header from '../../Header/Header'
 import { TaskForm } from '../../TaskForm/TaskForm'
 import Card from '../../Card/Card'
 
-import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
+import Skeleton from 'react-loading-skeleton'
+import { Radio, RadioGroup, FormControl, FormControlLabel } from '@mui/material'
 
 const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
 
 export const Tasks = () => {
   const [list, setList] = useState([])
+  const [filteredList, setFilteredList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const { isPhone } = useResize()
-
-  const renderAllTasks = () =>
-    list.map(task => <Card key={task._id} data={task} />)
-
-  const renderNewTasks = () =>
-    list
-      .filter(task => task.status === 'NEW')
-      .map(task => <Card key={task._id} data={task} />)
-
-  const renderInProgressTasks = () =>
-    list
-      .filter(task => task.status === 'IN PROGRESS')
-      .map(task => <Card key={task._id} data={task} />)
-
-  const renderFinishedTasks = () =>
-    list
-      .filter(task => task.status === 'FINISHED')
-      .map(task => <Card key={task._id} data={task} />)
 
   const fetchTasks = async () => {
     setIsLoading(true)
@@ -44,11 +28,45 @@ export const Tasks = () => {
       })
       const data = await res.json()
       setList(data.result)
+      setFilteredList(data.result)
       setIsLoading(false)
     } catch (err) {
       console.log(err)
       setIsLoading(false)
     }
+  }
+
+  const renderAllTasks = () =>
+    filteredList.map(task => <Card key={task._id} data={task} />)
+
+  const renderNewTasks = () =>
+    filteredList
+      .filter(task => task.status === 'NEW')
+      .map(task => <Card key={task._id} data={task} />)
+
+  const renderInProgressTasks = () =>
+    filteredList
+      .filter(task => task.status === 'IN PROGRESS')
+      .map(task => <Card key={task._id} data={task} />)
+
+  const renderFinishedTasks = () =>
+    filteredList
+      .filter(task => task.status === 'FINISHED')
+      .map(task => <Card key={task._id} data={task} />)
+
+  const handleChangeImportance = e => {
+    if (e.target.value === 'ALL') return setFilteredList(list)
+    setFilteredList(list.filter(task => task.importance === e.target.value))
+  }
+
+  const handleChangeWho = e => {
+    if (e.target.value === 'ME')
+      return setFilteredList(
+        list.filter(
+          task => task.user.userName === localStorage.getItem('userName')
+        )
+      )
+    setFilteredList(list)
   }
 
   useEffect(() => {
@@ -64,15 +82,53 @@ export const Tasks = () => {
           <div className="list_header">
             <h2>Mis tareas</h2>
           </div>
+          <div className="filters">
+            <FormControl>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                onChange={handleChangeWho}
+              >
+                <FormControlLabel
+                  value="ALL"
+                  control={<Radio />}
+                  label="Todas"
+                />
+                <FormControlLabel
+                  value="ME"
+                  control={<Radio />}
+                  label="Mis tareas"
+                />
+              </RadioGroup>
+            </FormControl>
+            <div className="search">
+              <input
+                type="text"
+                placeholder="Buscar por título..."
+                onChange={() => {}}
+              />
+            </div>
+            <select name="importance" onChange={handleChangeImportance}>
+              <option value="">Seleccionar una prioridad</option>
+              <option value="ALL">Todas</option>
+              <option value="LOW">Baja</option>
+              <option value="MEDIUM">Media</option>
+              <option value="HIGH">Alta</option>
+            </select>
+          </div>
           {isLoading ? (
             <Skeleton count={10} />
           ) : isPhone ? (
             <div className="list phone">
-              {!list.length ? <p>No hay tareas creadas</p> : renderAllTasks()}
+              {!filteredList.length ? (
+                <p>No hay tareas creadas</p>
+              ) : (
+                renderAllTasks()
+              )}
             </div>
           ) : (
             <div className="list_group">
-              {!list.length ? (
+              {!filteredList.length ? (
                 <p>No hay tareas creadas</p>
               ) : (
                 <>
